7bcd8fd6f6618500a2add698204680f2506b7f49
Bobby Allen <bobbyallen.uk@gmail.com>
2013-03-20 00:58:37 +0000

    Changes made to the library to ensure it meets PSR standards (covered 0 and 1 at the moment, need to finished PSR-2 tomorrow!)

--- examples/index.php
+++ examples/index.php
@@ -2,21 +2,23 @@
 
 /**
  * Distical
- * 
+ *
  * Distical is a simple distance calculator library for PHP 5.3+ which
  * amongst other things can calculate the distance between two lat/long
  * co-ordinates.
- * 
+ *
  * @author bobbyallen.uk@gmail.com (Bobby Allen)
  * @version 1.0.0
  * @license http://www.gnu.org/licenses/gpl.html
  * @link https://github.com/bobsta63/distical
- * 
+ *
  */
 /**
  * ---- EXAMPLE LIBRARY USAGE ----
  */
-require_once '../distical.class.php';
+require_once '../src/Distical/Calculator.php';
+
+use Distical\Calculator;
 
 #$points = array(
 #    'a' => array('lat' => 52.055868, 'lon' => 1.161804), // Central Ipswich!
@@ -28,22 +30,21 @@ $points = array(
     'b' => array('lat' => 51.81259469696908, 'lon' => -0.8111858367919922) // Central Aylesbury!
 );
 
-$point_to_point = new distical($points); // Create a new instance of the class.
+$point_to_point = new Calculator($points); // Create a new instance of the class.
 #$point_to_point->between($points); // Register the points (A to B etc!) as an alternative to passing it in with the class initiation string.
 
-$point_to_point->as_miles(); // Calculate as 'miles' as opposed to 'km'.
+$point_to_point->asMiles(); // Calculate as 'miles' as opposed to 'km'.
 $point_to_point->calculate(); // Calcluate the distance and store it ready to 'display()'!
 
-echo "Total distance = " . $point_to_point->display() . " " . $point_to_point->unit_of_measure() . "<br><br>";
+echo "Total distance = " . $point_to_point->display() . " " . $point_to_point->unitOfMeasure() . "<br><br>";
 
 /**
  * Lets checks if the total distance are greater than a defined distance.
  */
 $check_distance = 10;
 
-if ($point_to_point->check_greater_than($check_distance)) {
-    echo $point_to_point->display() . $point_to_point->unit_of_measure() . " is greater than " . $check_distance . " " . $point_to_point->unit_of_measure();
+if ($point_to_point->checkGreaterThan($check_distance)) {
+    echo $point_to_point->display() . $point_to_point->unitOfMeasure() . " is greater than " . $check_distance . " " . $point_to_point->unitOfMeasure();
 } else {
-    echo $point_to_point->display() . $point_to_point->unit_of_measure() . " is less than " . $check_distance . " " . $point_to_point->unit_of_measure();
+    echo $point_to_point->display() . $point_to_point->unitOfMeasure() . " is less than " . $check_distance . " " . $point_to_point->unitOfMeasure();
 }
-?>
--- src/Distical/Calculator.php
+++ src/Distical/Calculator.php
@@ -2,27 +2,29 @@
 
 /**
  * Distical
- * 
+ *
  * Distical is a simple distance calculator library for PHP 5.3+ which
  * amongst other things can calculate the distance between two lat/long
  * co-ordinates.
- * 
+ *
  * @author bobbyallen.uk@gmail.com (Bobby Allen)
  * @version 1.0.0
  * @license http://www.gnu.org/licenses/gpl.html
  * @link https://github.com/bobsta63/distical
- * 
+ *
  */
-class Distical {
+namespace Distical;
+
+class Calculator
+{
     /**
      * Stores the earth's mean radius, used by the calculate() method.
      */
-
-    const mean_earth_radius = 6372.797;
+    const MEAN_EARTH_RADIUS = 6372.797;
 
     /**
      * Stores the format to report distance on.
-     * @var string Format prefix (valid options are 'm' for miles or 'k' for kilometres) 
+     * @var string Format prefix (valid options are 'm' for miles or 'k' for kilometres)
      */
     private $format;
 
@@ -51,10 +53,11 @@ class Distical {
     private $total = 0.0;
 
     /**
-     * The constructor (enough said!)
+     * The constructor
      * @param array $points_array Optional inital points array.
      */
-    public function __construct($points_array = null) {
+    public function __construct($points_array = null)
+    {
         if ($points_array != null) {
             $this->between($points_array);
         } else {
@@ -69,7 +72,8 @@ class Distical {
      * Setter to register lat/long points for 'A' and 'B'.
      * @param array $points_array A multi-dimensional array of 'A' and 'B' point lat/long values.
      */
-    public function between($points_array) {
+    public function between($points_array)
+    {
         $points_object = json_decode(json_encode($points_array));
         $this->a = $points_object->a;
         $this->b = $points_object->b;
@@ -78,7 +82,8 @@ class Distical {
     /**
      * Change the format for results to 'Miles'
      */
-    public function as_miles() {
+    public function asMiles()
+    {
         $this->human_format = 'miles';
         $this->format = 'm';
     }
@@ -86,7 +91,8 @@ class Distical {
     /**
      * Change the format for results to 'Kilometres'
      */
-    public function as_km() {
+    public function asKilometres()
+    {
         $this->human_format = 'kilometres';
         $this->format = 'k';
     }
@@ -95,14 +101,16 @@ class Distical {
      * Display the huamn readable format.
      * @return string The human readable format as a string.
      */
-    public function unit_of_measure() {
+    public function unitOfMeasure()
+    {
         return $this->human_format;
     }
 
     /**
      * Does the actual calculation between the two lat/long points.
      */
-    public function calculate() {
+    public function calculate()
+    {
         $pi80 = M_PI / 180;
 
         $this->a->lat *= $pi80;
@@ -114,16 +122,17 @@ class Distical {
         $dlng = $this->b->lon - $this->a->lon;
         $a = sin($dlat / 2) * sin($dlat / 2) + cos($this->a->lat) * cos($this->b->lat) * sin($dlng / 2) * sin($dlng / 2);
         $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-        $this->total = self::mean_earth_radius * $c;
+        $this->total = self::MEAN_EARTH_RADIUS * $c;
     }
 
     /**
      * A helper method to check if the calculated distance is greater than a
      * specfied distance.
-     * @param int $distance The distance to check against.
+     * @param  int     $distance The distance to check against.
      * @return boolean
      */
-    public function check_greater_than($distance) {
+    public function checkGreaterThan($distance)
+    {
         if ($this->total > $distance)
             return true;
         return false;
@@ -133,7 +142,8 @@ class Distical {
      * Returns the total distance between the two lat/lng points.
      * @return decimal The total distance between both points.
      */
-    public function display() {
+    public function display()
+    {
         switch ($this->format) {
             case 'm':
                 $out_total = $this->total * 0.621371192;
@@ -142,9 +152,8 @@ class Distical {
                 $out_total = $this->total;
                 break;
         }
+
         return $out_total;
     }
 
 }
-
-?>
